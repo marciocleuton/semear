8541ffa89bab0864024cdbccf413e2cbbd668269
Márcio Cleuton da Costa Pereira <marcio.cleuton@gmail.com>
2021-11-29 23:43:38 -0300

    Testando o dominio

--- src/main/java/com/semear/resources/CategoriaResources.java
+++ src/main/java/com/semear/resources/CategoriaResources.java
@@ -4,12 +4,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.semear.dominio.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String lista() {
-		return "ok";
+	public List<Categoria> lista() {
+		 Categoria cat1 = new Categoria();
+		 Categoria cat2 = new Categoria();
+		 
+		 
+		 cat1.setId(1);
+		 cat1.setNome("informatica");
+		 
+		 cat2.setId(2);
+		 cat2.setNome("Escritorio");
+		 
+		 List<Categoria> listaCat = new ArrayList();
+		 
+		 
+		 
+		 
+		 listaCat.add(cat1);
+		 listaCat.add(cat2);
+		
+		return listaCat;
 	}
 }
